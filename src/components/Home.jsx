278d86f904db90764525087d933618b96b72eea1
UmeshMehta1 <ukmehta1245@gmail.com>
2024-05-06 10:00:05 +0545

    changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -15,86 +15,93 @@ import { FaNodeJs } from "react-icons/fa6";
 import { ReactTyped } from "react-typed";
 
 function Home() {
-    return (
-        <>
-            <div
-                name="Home"
-                className="max-w-screen-2xl container mx-auto px-4 md:px-20 my-28"
-            >
-                <div className="flex flex-col md:flex-row">
-                    <div className="md:w-1/2 mt-12 md:mt-24 space-y-2 order-2 md:order-1">
-                        <span className="text-xl">Welcome In My Feed</span>
-                        <div className="flex space-x-1 text-2xl md:text-4xl">
-                            <h1>Hello, I'm a</h1>
-                            {/* <span >Developer</span> */}
-                            <ReactTyped
-                                className="text-red-700 font-bold"
-                                strings={["Developer", "Programmer", "Coder"]}
-                                typeSpeed={40}
-                                backSpeed={50}
-                                loop={true}
-                            />
-                        </div>
-                        <br />
-                        <p className="text-sm md:text-md text-justify">
-                            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                            Quisquam, recusandae consequatur. Accusamus sint libero eligendi
-                            laborum fuga repudiandae? Asperiores tempore eos animi dolores
-                            corrupti! Sint quos, mollitia architecto aliquam nesciunt, optio
-                            cum delectus sit voluptatum aut ratione quaerat veniam aperiam!
-                        </p>
-                        <br />
-                        {/* social media icons */}
-                        <div className="flex flex-col items-center md:flex-row justify-between space-y-6 md:space-y-0">
-                            <div className="  space-y-2">
-                                <h1 className="font-bold text-center ">Available on</h1>
-                                <ul className="flex space-x-5">
-                                    <li>
-                                        <a href="https://www.facebook.com/umesh.mehta.980967" target="_blank">
-                                            <FaSquareFacebook className="text-2xl cursor-pointer" />
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="https://www.linkedin.com/" target="_blank">
-                                            <FaLinkedin className="text-2xl cursor-pointer" />
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="https://www.youtube.com/" target="_blank">
-                                            <IoLogoYoutube className="text-2xl cursor-pointer" />
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="https://t.me/" target="_blank">
-                                            <FaTelegram className="text-2xl cursor-pointer" />
-                                        </a>
-                                    </li>
-                                </ul>
-                            </div>
-                            <div className=" space-y-2">
-                                <h1 className="font-bold text-center">Currently working on</h1>
-                                <div className="flex space-x-5">
-                                    <SiMongodb className="text-2xl md:text-3xl hover:scale-110 duration-200 rounded-full border-[2px] cursor-pointer" />
-                                    <SiExpress className="text-2xl md:text-3xl hover:scale-110 duration-200 rounded-full border-[2px] cursor-pointer" />
-                                    <FaReact className="text-2xl md:text-3xl hover:scale-110 duration-200 rounded-full border-[2px] cursor-pointer" />
-                                    <FaNodeJs className="text-2xl md:text-3xl hover:scale-110 duration-200 rounded-full border-[2px] cursor-pointer" />
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="md:w-1/2 md:ml-48 md:mt-20 mt-8 order-1">
-                        <img
-                            src={pic}
-                            className="rounded-full md:w-[450px] md:h-[450px]"
-                            alt=""
-                        />
-                    </div>
-                </div>
+  return (
+    <>
+      <div
+        name="Home"
+        className="max-w-screen-2xl container mx-auto px-4 md:px-20 my-28"
+      >
+        <div className="flex flex-col md:flex-row">
+          <div className="md:w-1/2 mt-12 md:mt-24 space-y-2 order-2 md:order-1">
+            <span className="text-xl">Welcome In My Feed</span>
+            <div className="flex space-x-1 text-2xl md:text-4xl">
+              <h1>Hello, I'm a</h1>
+              {/* <span >Developer</span> */}
+              <ReactTyped
+                className="text-red-700 font-bold"
+                strings={["Developer", "Programmer", "Coder"]}
+                typeSpeed={40}
+                backSpeed={50}
+                loop={true}
+              />
             </div>
+            <br />
+            <p className="text-sm md:text-md text-justify">
+              My name is Umesh Mehta, and I'm a skilled MERN stack developer.
+              Specializing in MongoDB, Express.js, React.js, and Node.js, I'm
+              passionate about crafting dynamic and user-centric web
+              applications. With expertise in React.js and Node.js, I bring
+              concepts to life, ensuring seamless functionality and exceptional
+              user experiences. Committed to staying abreast of industry trends,
+              I continuously refine my skills to deliver cutting-edge solutions.
+              With a proven track record of excellence, I'm dedicated to pushing
+              the boundaries of web development and making a meaningful impact
+              in the field.
+            </p>
+            <br />
+            {/* social media icons */}
+            <div className="flex flex-col items-center md:flex-row justify-between space-y-6 md:space-y-0">
+              <div className="  space-y-2">
+                <h1 className="font-bold text-center ">Available on</h1>
+                <ul className="flex space-x-5">
+                  <li>
+                    <a
+                      href="https://www.facebook.com/umesh.mehta.980967"
+                      target="_blank"
+                    >
+                      <FaSquareFacebook className="text-2xl cursor-pointer" />
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="https://www.linkedin.com/in/umeshmehta1245/"
+                      target="_blank"
+                    >
+                      <FaLinkedin className="text-2xl cursor-pointer" />
+                    </a>
+                  </li>
 
-            <hr />
-        </>
-    );
+                  <li>
+                    <a href="https://t.me/" target="_blank">
+                      <FaTelegram className="text-2xl cursor-pointer" />
+                    </a>
+                  </li>
+                </ul>
+              </div>
+              <div className=" space-y-2">
+                <h1 className="font-bold text-center">Currently working on</h1>
+                <div className="flex space-x-5">
+                  <SiMongodb className="text-2xl md:text-3xl hover:scale-110 duration-200 rounded-full border-[2px] cursor-pointer" />
+                  <SiExpress className="text-2xl md:text-3xl hover:scale-110 duration-200 rounded-full border-[2px] cursor-pointer" />
+                  <FaReact className="text-2xl md:text-3xl hover:scale-110 duration-200 rounded-full border-[2px] cursor-pointer" />
+                  <FaNodeJs className="text-2xl md:text-3xl hover:scale-110 duration-200 rounded-full border-[2px] cursor-pointer" />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="md:w-1/2 md:ml-48 md:mt-20 mt-8 order-1">
+            <img
+              src={pic}
+              className="rounded-full md:w-[450px] md:h-[450px]"
+              alt=""
+            />
+          </div>
+        </div>
+      </div>
+
+      <hr />
+    </>
+  );
 }
 
 export default Home;
